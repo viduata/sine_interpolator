de178f9ffe558b561476d944fb4009d3e20b2ba1
Jeremy Matters <jeremy.matters@iamplus.com>
2019-03-13 17:05:16 -0700

    small changes to codebase. fake functions added

--- interpolation.c
+++ interpolation.c
@@ -2,6 +2,12 @@
 #include "sine_table.h"
 #include <math.h>
 
+
+///////////////////////////////////////////////////////////////////////////////
+// sine_interpolator()
+// Given a value (in degrees), this uses a pre-baked table of sine values
+// to give an approximation of sine.
+// The method used is a simple first-order linear approximation
 float sine_interpolator(float input)
 {
 	int left = floor(input);
@@ -10,4 +16,31 @@ float sine_interpolator(float input)
 	input = fmod(input, 360);
 
 	return (input*sine_table[(left+1)%360] - input*sine_table[left] + sine_table[left]*(left+1) - (left*sine_table[(left+1)%360]));
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// saw_generator()
+// 
+float saw_generator(float input)
+{
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// square_generator()
+// 
+float square_generator(float input)
+{
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// triangle_generator()
+// 
+float triangle_generator(float input)
+{
+	return 1;
 }
