74a5c98eb069688bcf005ec446dff1ce32e25196
Jeremy Matters <jeremy.matters@iamplus.com>
2019-03-15 16:53:41 -0700

    tests with new wave types

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ void wave_recalculate(oscillator_t * the_wave);
 // GLOBAL VARIABLES
 oscillator_t principle_wave;
 oscillator_t lf_modulator;
+oscillator_t secondary_wave;
 
 int main (int argc, char * arg[])
 {
@@ -26,14 +27,16 @@ int main (int argc, char * arg[])
 	int 	samples = SAMPLE_RATE * 5; // Create five seconds of audio
 	float 	root_frequency = 440;
 	float 	mod_frequency  = 440;
-	float 	lfo_frequency  = 40;
-	float 	lfo_depth      = 40;
+	float 	lfo_frequency  = 5;
+	float 	lfo_depth      = 150;
 	int 	intvalue       = 0;
 	float 	value          = 0;
 	long    x			   = 0;
 
-	wave_init (&principle_wave, root_frequency, 440, 0); 	// Principle sine wave at 440 Hz
+	wave_init (&principle_wave, SINE, root_frequency, 0); 	// Principle sine wave at 440 Hz
 	wave_init (&lf_modulator, UNIT_SINE, lfo_frequency, 0); // LFO FM oscillator at 2 Hz
+	wave_init (&secondary_wave, UNIT_SINE, 500, 0); // LFO FM oscillator at 2 Hz
+
 
 	// Open file for writing
 	fh = fopen(filename, "w");
@@ -41,52 +44,17 @@ int main (int argc, char * arg[])
 	// Everything happens in one loop
 	for (x=0; x<samples; x++)
 	{
-		value = sine_interpolator(principle_wave.position);
+		value = triangle_generator(principle_wave.position) + saw_generator(secondary_wave.position);
 		intvalue = (int16_t)value;
 		principle_wave.frequency = root_frequency + (unit_sine(lf_modulator.position)*lfo_depth);
-		wave_recalculate(&principle_wave);
+		wave_recalculate(&principle_wave); // required if a value is modified
 		wave_increment(&principle_wave);
 		wave_increment(&lf_modulator);
+		wave_increment(&secondary_wave);
 
 		printf("%ld, %f\r\n", x, value); 
 		fwrite(&intvalue,sizeof(int16_t), 1, fh);
-
-		// value = sine_interpolator(wave_position); // gives a sine wave
-		// intvalue = (int16_t)value;
-
-		// printf("%f, %f\r\n", x, value); 
-		// fwrite(&intvalue,sizeof(int16_t), 1, fh);
-		// wave_position += interval;
-
-		// value = sine_interpolator(wave_position); // gives a sine wave
-		// printf("%f, %f\r\n", x, saw_generator(x*interval)); // gives a square wave
-		// printf("%f, %f\r\n", x, triangle_generator(x*interval)); // gives a triangle wave
 	}
-
-	// frequency *= 1.5;
-	// interval = 360/(sample_rate / frequency);
-
-	// for (x=0; x<samples; x++)
-	// {
-	// 	value = sine_interpolator(wave_position);// gives a sine wave
-	// 	intvalue = (int16_t)value;
-	// 	printf("%f, %f\r\n", x, value); 
-	// 	fwrite(&intvalue,sizeof(int16_t), 1, fh);
-	// 	wave_position += interval;
-	// }
-
-	// frequency *= 1.5;
-	// interval = 360/(sample_rate / frequency);
-
-	// for (x=0; x<samples; x++)
-	// {
-	// 	value = sine_interpolator(wave_position);// gives a sine wave
-	// 	intvalue = (int16_t)value;
-	// 	printf("%f, %f\r\n", x, value); 
-	// 	fwrite(&intvalue,sizeof(int16_t), 1, fh);
-	// 	wave_position += interval;
-	// }
-
 	fclose(fh);
 
 	return 0;
@@ -115,29 +83,3 @@ void wave_increment(oscillator_t * the_wave)
 }
 
 
-	// float x=0;
-	// float duration;
-	// float sample_rate;
-	// float frequency;
-	// int   samples;
-	// float interval;
-
-	// FILE * fh;
-	// float value = 0;
-	// int16_t intvalue = 0;
-	// float  wave_position = 0;
-
-	// printf("Starting interpolator\r\n");
-
-	// if(argc!=5)
-	// {
-	// 	printf("Usage: %s <freq> <duration> <sample rate> <outfile>\r\n", arg[0]);
-	// 	exit(1);
-	// }
-
-	// sample_rate = atof(arg[3]);
-	// frequency   = atof(arg[1]);
-	// duration    = atof(arg[2]);
-
-	// samples  = floor( sample_rate * duration );
-	// interval = 360/(sample_rate / frequency);
